228cbb4afd9713062f2636d65daaaace0bd97ffb
barath <mabarathkumar@gmail.com>
2025-07-08 20:42:57 +0530

    scoll change

--- script.js
+++ script.js
@@ -6,3 +6,8 @@ nav_bar.addEventListener('click',()=>{
   nav_bar.classList.toggle('active')
   nav_menu.classList.toggle('active')
 })
+
+// Disable scroll restoration so the browser doesn't jump to previous scroll position
+if ('scrollRestoration' in history) {
+  history.scrollRestoration = 'manual';
+}
