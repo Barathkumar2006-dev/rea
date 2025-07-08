91921e361d67bc517a6752d5f02b80808de4623b
barath <mabarathkumar@gmail.com>
2025-07-08 21:45:24 +0530

    change

--- script.js
+++ script.js
@@ -7,17 +7,3 @@ nav_bar.addEventListener('click',()=>{
   nav_menu.classList.toggle('active')
 })
 
-document.querySelectorAll('.nav_menu a').forEach(link => {
-  link.addEventListener('click', function(e) {
-    e.preventDefault(); // Stop default anchor behavior
-    const targetId = this.getAttribute('href').substring(1);
-    const targetSection = document.getElementById(targetId);
-    
-    // Smooth scroll manually
-    targetSection?.scrollIntoView({ behavior: 'smooth' });
-
-    // Close mobile menu if open
-    document.querySelector('.nav_menu').classList.remove('active');
-    document.querySelector('.toggle').classList.remove('active');
-  });
-});
