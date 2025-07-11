1578e4c46ab1cb05118a1d16038b7e957ddca015
barath kumar <mabarathkumar@gmail.com>
2025-07-12 01:39:14 +0530

    script.js

--- script.js
+++ script.js
@@ -14,4 +14,13 @@ const menu_bar = document.querySelector('.nav_menu')
 toggle.addEventListener('click',()=>{
   toggle.classList.toggle('active')
   menu_bar.classList.toggle('active')
+
 })
+
+document.addEventListener('click', (e) => {
+  if (!menu_bar.contains(e.target) && !toggle.contains(e.target)) {
+    toggle.classList.remove('active');
+    menu_bar.classList.remove('active');
+  }
+});
+
